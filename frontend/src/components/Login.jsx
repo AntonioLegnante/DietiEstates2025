e62711c011a9e5893e92cdf6d3678d621ee24ddd
AntonioLegnante <Antoniolegnante12@gmail.com>
2025-11-04 10:10:42 +0100

    Aggiunto context in react, sistemazione tabelle congiunte

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,33 +1,3 @@
-/*import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { login } from './AuthContext.jsx';
-import axios from 'axios';
-
-export function Login() {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-
-    const navigate = useNavigate();
-
-    function handleLoginButton() {
-        axios.post("auth/login", {username, password})
-        .then(response => {
-            console.log(response.data.token)
-            login(response.data.token);
-            navigate("/");
-        });
-    }
-
-    return (
-        <div>
-            <label htmlFor="username">Username</label>
-            <input id="username" onChange={e => setUsername(_ => e.target.value)}></input><br/>
-            <label htmlFor="password">Password</label>
-            <input type="password" id="password" onChange={e => setPassword(_ => e.target.value)}></input><br/>
-            <button onClick={handleLoginButton}>Accedi</button>
-        </div>
-    )
-}*/
 
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';

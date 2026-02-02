c0b0e45ee78552a13cbd5309f7088933c842856b
Vincenzo Noviello <vincio40@gmail.com>
2026-02-02 11:48:30 +0100

    Corretta la query, ora trova tutti gli immobili che hanno una sottostringa equivalente a ciò che inserito l'utente e il filtro vendita e affitto

--- DietiEstates2025/src/main/java/com/DietiEstates2025/DietiEstates2025/Repositories/ImmobileRepository.java
+++ DietiEstates2025/src/main/java/com/DietiEstates2025/DietiEstates2025/Repositories/ImmobileRepository.java
@@ -9,30 +9,31 @@ import java.util.List;
 
 public interface ImmobileRepository extends JpaRepository<Immobile, Integer> {
     @Query("""
-        SELECT i FROM Immobile i
-        WHERE ((:localita IS NULL OR i.indirizzo LIKE :localita)
-              OR (:localita IS NULL OR i.citta LIKE :localita))
-                     
-        AND ((i.prezzo <= :maxPrezzo AND i.prezzo >= :minPrezzo) 
-            OR (:maxPrezzo IS NULL AND i.prezzo >= :minPrezzo) 
-            OR (:minPrezzo IS NULL AND i.prezzo <= :maxPrezzo)
-            OR (:minPrezzo IS NULL AND :maxPrezzo IS NULL))
-                
-        AND (
-            (:affitta IS NOT NULL AND :affitta = TRUE AND i.affitto = TRUE)
-            OR (:vendita IS NOT NULL AND :vendita = TRUE AND i.vendita = TRUE)
-            OR (:affitta IS NULL AND :vendita IS NULL)
-         )
-         
-         AND (:numeroStanze IS NULL OR i.numeroStanze >= :numeroStanze)
-         
-         AND (:dimensione IS NULL OR i.dimensione >= :dimensione)
-         
-         AND (:piano IS NULL OR i.piano = :piano)
-         
-         AND (:classeEnergetica IS NULL OR i.classeEnergetica = :classeEnergetica)
-         
-         AND (:numeroBagni IS NULL OR i.numeroBagni >= :numeroBagni)
+    SELECT i FROM Immobile i
+    WHERE (:localita IS NULL 
+          OR LOWER(i.indirizzo) LIKE LOWER(CONCAT('%', :localita, '%'))
+          OR LOWER(i.citta) LIKE LOWER(CONCAT('%', :localita, '%')))
+                 
+    AND ((i.prezzo <= :maxPrezzo AND i.prezzo >= :minPrezzo) 
+        OR (:maxPrezzo IS NULL AND i.prezzo >= :minPrezzo) 
+        OR (:minPrezzo IS NULL AND i.prezzo <= :maxPrezzo)
+        OR (:minPrezzo IS NULL AND :maxPrezzo IS NULL))
+            
+    AND (
+        (:affitta = TRUE AND i.affitto = TRUE)
+        OR (:vendita = TRUE AND i.vendita = TRUE)
+        OR (:affitta = FALSE AND :vendita = FALSE)
+     )
+     
+     AND (:numeroStanze IS NULL OR i.numeroStanze >= :numeroStanze)
+     
+     AND (:dimensione IS NULL OR i.dimensione >= :dimensione)
+     
+     AND (:piano IS NULL OR i.piano = :piano)
+     
+     AND (:classeEnergetica IS NULL OR i.classeEnergetica = :classeEnergetica)
+     
+     AND (:numeroBagni IS NULL OR i.numeroBagni >= :numeroBagni)
     """)
     List<Immobile> ricercaAvanzata(
             @Param("localita") String localita,
